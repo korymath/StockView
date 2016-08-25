4f6733b90139d4b3e1dab912e6cfa4f5aaa46366
Artem Sobolev <asobolev@twitter.com>
2016-08-25 15:13:52 -0700

    Initial finance api stuff

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -8,4 +8,15 @@ chrome.tabs.onUpdated.addListener(function (tabId) {
   chrome.pageAction.show(tabId);
 });
 
-console.log('\'Allo \'Allo! Event Page for Page Action');
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    
+    $.get('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22' + request.symbol + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function(data) {
+      sendResponse({data : data});
+    });
+    return true;
+  });
+
